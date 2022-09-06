3fde80de7b815bd105066516b6044b6e2da2e32b
steve <steve@debian-BULLSEYE-live-builder-AMD64>
2022-09-06 16:16:30 -0600

    update monthly script

--- summarize_monthly.R
+++ summarize_monthly.R
@@ -3,13 +3,14 @@ library(ggplot2)
 library(plotly)
 library(xts)
 library(ggpubr)
-
-panamint_site_ids <- paste(as.character(seq(219, 243)), ".csv", sep="")
-wah_wah_site_ids <- c("497.csv") #paste(as.character(seq(497, 498)), ".csv", sep="")
-silver_peak_site_ids <- paste(as.character(seq(358, 361)), ".csv", sep="")
-rawhide_site_ids <- paste(as.character(seq(154, 165)), ".csv", sep="")
+library(readxl)
 
 find_loc <- function(x) {
+    #' Lookup site.csv in locations to determine if in region of interest
+    panamint_site_ids <- paste(as.character(seq(219, 243)), ".csv", sep="")
+    wah_wah_site_ids <- c("497.csv") #paste(as.character(seq(497, 498)), ".csv", sep="")
+    silver_peak_site_ids <- paste(as.character(seq(358, 361)), ".csv", sep="")
+    rawhide_site_ids <- paste(as.character(seq(154, 165)), ".csv", sep="")
     case_when(x %in% panamint_site_ids ~ "panamint",
               x %in% wah_wah_site_ids ~ "wah_wah",
               x %in% silver_peak_site_ids ~ "silver_peak",
@@ -17,9 +18,13 @@ find_loc <- function(x) {
               TRUE ~ "other")
 }
 
+sites_tbl <- read_excel("sites.xlsx")
+
+setwd("./monthly")
+
 tbl <-
     list.files(pattern = "*.csv") %>%
-    map_df(~read_csv(.)) %>%
+    map_df(~read_csv(., col_types = cols())) %>%
     separate(`Row Labels`, c("year", "month")) %>%
     group_by(site)
 
@@ -30,17 +35,21 @@ summary_tbl <- tbl %>%
         mean_AET_pre_2010 = mean(`Sum of AET`[year <= 2010]),
         mean_D_pre_2010 = mean(`Sum of D`[year <= 2010]),
         mean_GDD_pre_2010 = mean(`Sum of GDD`[year <= 2010]),
+        mean_SOIL_pre_2010 = mean(`Average of SOIL`[year <= 2010]),
 ###
         mean_t_post_2010 = mean(`Average of T`[year > 2010]),
         mean_P_post_2010 = mean(`Sum of P`[year > 2010]),
         mean_AET_post_2010 = mean(`Sum of AET`[year > 2010]),
         mean_D_post_2010 = mean(`Sum of D`[year > 2010]),
         mean_GDD_post_2010 = mean(`Sum of GDD`[year > 2010]),
+        mean_SOIL_post_2010 = mean(`Average of SOIL`[year > 2010]),
     ) %>%
-    filter(mean_AET_pre_2010 <= 200 | mean_AET_post_2010 <=200) %>%
+#    filter(mean_AET_pre_2010 <= 200 | mean_AET_post_2010 <=200) %>%
     mutate(loc = fct_relevel(find_loc(site), c("panamint", "wah_wah", "silver_peak", "rawhide", "other"))) %>%
     group_by(loc) %>%
-    arrange(desc(loc))
+    arrange(desc(loc)) %>%
+    left_join(sites_tbl, by = c("site" = "Site")) 
+
 
 loc_colors = c("red", "green", "aquamarine", "darkorchid1", "black")
 
@@ -67,6 +76,14 @@ gdd_plt_pre_2010 <-summary_tbl %>%
     geom_point(mapping = aes(x = mean_t_pre_2010, y = mean_GDD_pre_2010, color = loc)) +
     labs(x = "Mean Annual Temperature C (1980-2010)", y = "GDD") +
     scale_color_manual(name = 'Location', values = loc_colors)
+
+soil_plt_pre_2010 <-summary_tbl %>%
+    ggplot(mapping = aes(label = site)) +
+    geom_point(mapping = aes(x = mean_t_pre_2010, y = mean_SOIL_pre_2010, color = loc)) +
+    labs(x = "Mean Annual Temperature C (1980-2010)", y = "SOIL") +
+    scale_color_manual(name = 'Location', values = loc_colors)
+
+soil_plt_pre_2010
 
 figure_pre_2010 <- ggarrange(cwd_plt_pre_2010, p_plt_pre_2010, aet_plt_pre_2010, gdd_plt_pre_2010,
                              common.legend = TRUE,
@@ -98,8 +115,20 @@ gdd_plt_post_2010 <-summary_tbl %>%
     labs(x = "Mean Annual Temperature C (2011-2020)", y = "GDD") +
     scale_color_manual(name = 'Location', values = loc_colors)
 
+soil_plt_post_2010 <-summary_tbl %>%
+    ggplot(mapping = aes(label = site)) +
+    geom_point(mapping = aes(x = mean_t_pre_2010, y = mean_SOIL_post_2010, color = loc)) +
+    labs(x = "Mean Annual Temperature C (2011-2020)", y = "SOIL") +
+    scale_color_manual(name = 'Location', values = loc_colors)
+
+soil_plt_post_2010
+
 figure_post_2010 <- ggarrange(cwd_plt_post_2010, p_plt_post_2010, aet_plt_post_2010, gdd_plt_post_2010,
                              common.legend = TRUE,
                              labels = c("A", "B", "C", "D"))
 
 figure_post_2010
+
+
+# 3d plot example
+plot_ly(x = summary_tbl$mean_t_post_2010, y = summary_tbl$mean_P_post_2010, z = summary_tbl$mean_SOIL_post_2010,  type="scatter3d", mode="markers", color = summary_tbl$loc)
