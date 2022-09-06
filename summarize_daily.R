544d95b970c84515107f166b863d063784b9de59
steve <steve@debian-BULLSEYE-live-builder-AMD64>
2022-09-06 16:16:35 -0600

    update daily script

--- summarize_daily.R
+++ summarize_daily.R
@@ -1,7 +1,10 @@
 library(tidyverse)
+library(tsibble)
+library(tsibbledata)
 library(ggplot2)
 library(plotly)
 library(xts)
+library(fable)
 
 panamint_site_ids <- paste(as.character(seq(219, 243)), ".csv", sep="")
 wah_wah_site_ids <- c("497.csv") #paste(as.character(seq(497, 498)), ".csv", sep="")
@@ -15,11 +18,21 @@ find_loc <- function(x) {
               TRUE ~ "other")
 }
 
-tbl <-
+daily_tbl <-
     list.files(pattern = "*.csv") %>%
-    map_df(~read_csv(.)) %>%
+    map_df(~read_csv(., col_types = cols())) %>%
+#    mutate(date = as.Date(date)) %>%
     group_by(site)
 
-tbl <- mutate(tbl, date = as.POSIXct(date, format = "%m/%d/%Y"))
+daily_tbl <- mutate(daily_tbl, date = as.POSIXct(date, format = "%m/%d/%Y"))
 
 tbl_xts <- as.xts(tbl)
+
+
+find_melt_date <- function(x) {
+    ts <- as_tsibble(x)
+    span = 3 # number od days of persistent snow used to identify snow season when working backward from summer
+    mdate <- 1 # set to 1 which evaluates to Jan 1 for years and places with no snow
+
+    return("test")
+}
